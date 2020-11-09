bdbab17267de6388a0f7bc109799452a964bd288
marcelochirai <65429412+marcelochirai@users.noreply.github.com>
2020-11-08 21:33:33 -0300

    Add files via upload
    A TELA DO PROGRAMA EXIBIRÁ AS INFORMAÇÕES ABAIXO:
    QUAL É A COTAÇÃO DO DÓLAR? 3.10
    QUANTOS DÓLARES VOCÊ IRÁ COMPRAR: 200.00
    VALOR A SER PAGO EM REAIS = 657.20
    
    O CONTEÚDO DESTA TAREFA ESTÁ NA AULA DO DIA 14/09/20.

---
+++ Calculo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace POO_DolarComIOF
+{
+    class Calculo
+    {
+        public double CotDolar;
+        public int QtdDolar;
+        public double QtdReal;
+
+        public double ValorCompra()
+        {
+            double totalDolar = QtdDolar * CotDolar;    //Valor sem IOF
+            return totalDolar + totalDolar * 6 / 100.0;     //Valor com IOF
+        }
+
+        public override string ToString()
+        {
+            return QtdReal
+                +" Valor a ser pago em Reais = "
+                + ValorCompra().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+
+
+}
+    
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace POO_DolarComIOF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculo dolar = new Calculo();
+            
+            Console.Write("Qual é a cotação do dólar?  ");
+            dolar.CotDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você irá comprar?  ");
+            dolar.QtdDolar = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write(dolar);
+
+            Console.ReadKey();
+        }
+
+    }
+}
